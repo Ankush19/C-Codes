9f0db7c133b3cd830e2ad78322355e82ff2c6c8b
dudani_a <dudani_a@ANKUSH-D>
2017-08-31 16:55:55 +0530

    .net-mvc

---
+++ Services/WebApplication/WebApplication/Controllers/MyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        [ActionName("GetString")]
+        public string GetString()
+        {
+            return "Hi!This is Ankush Dudani#Ankii";
+        }
+
+        public ActionResult GetData()
+        {
+            MyInfo info = new MyInfo();
+            info.Name = "Ankush";
+            info.MyId = 1;
+            info.Age = 22;
+            return View("MyView",info);
+
+        }
+    }
+}
---
+++ Services/WebApplication/WebApplication/Models/MyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class MyInfo
+    {
+        public int MyId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
