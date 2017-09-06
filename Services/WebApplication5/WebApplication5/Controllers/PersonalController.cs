4d79ca83930dd433fdfd74c13430f7720679bd10
dudani_a <dudani_a@ANKUSH-D>
2017-09-06 17:08:25 +0530

    Web Api

---
+++ Services/WebApplication5/WebApplication5/Controllers/PersonalController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class PersonalController : ApiController
+    {
+        // GET: Personal
+        public static List<Info> entries = new List<Info>()
+            {
+                new Info { MyID=1,Name="Ankush", Age=21 },
+                new Info { MyID=2, Name="Anki", Age=20},
+                new Info { MyID=3, Name="AnkiD", Age=40},
+
+            };
+
+        public IEnumerable<Info> Get()
+        {
+            return entries;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var entry = entries.FirstOrDefault((e) => e.MyID == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entry);
+
+        }
+
+        public IEnumerable<Info> Post([FromBody] Info a)
+        {
+            entries.Add(a);
+            return entries;
+        }
+    }
+}
---
+++ Services/WebApplication5/WebApplication5/Models/Info.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Info
+    {
+        public int MyID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
