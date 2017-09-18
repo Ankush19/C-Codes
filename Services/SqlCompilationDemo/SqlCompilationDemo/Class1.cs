c4934e59f9fdcca792eaed373506c719e6e06e67
dudani_a <dudani_a@ANKUSH-D>
2017-09-18 16:49:15 +0530

    SqlCompilation

---
+++ Services/SqlCompilationDemo/SqlCompilationDemo/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlCompilationDemo
+{
+    public static  class HelloUtil
+    {
+        [SqlFunction]
+        public static string AppendHello(string msg)
+        {
+            return msg + "::Hello:)";
+        }
+    }
+
+}
