5224294aece3d0acdc50c966df0ab4e9868088a9
dudani_a <dudani_a@ANKUSH-D>
2017-09-19 16:39:48 +0530

    SSIS And SSDT

---
+++ SSDTDemo/SSDTDemo/Procedure.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[Procedure]
+	@MyId int
+AS
+	SELECT * 
+	from dbo.myTable
+	where Myid=@MyId
+RETURN 0
---
+++ SSDTDemo/SSDTDemo/Script.PostDeployment1.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+insert into dbo.myTable(MyId,Name,Age)
+select 1,'Ankush',21
+where not exists (select 1 from dbo.myTable where MyId=1)
+go
