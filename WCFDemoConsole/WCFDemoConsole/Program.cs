3ffcc10daaab7863b3f91b61391eb13e6d08baf1
dudani_a <dudani_a@ANKUSH-D>
2017-09-21 16:56:32 +0530

    WCF Service Contract

---
+++ WCFDemoConsole/WCFDemoConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServiceReference1.StudentClient();
+            try
+            {
+                client.Open();
+                var students = client.GetStudents();
+                
+                foreach (var student in students)
+                {
+                    Console.WriteLine(string.Format("Name:{0},ID:{1}", student.Name, student.Id));
+                }
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            finally
+            {
+                client.Close();
+            }
+            
+        }
+    }
+}
---
+++ WcfServiceDemo/WcfServiceDemo/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Service1 : IStudent
+    {
+        private List<Student> _student = new List<Student>
+        {
+            new Student { Name="Abs",Id=1},
+            new Student { Name="Abc",Id=2}
+
+        };
+        
+        public List<Student> GetStudents()
+        {
+            return _student;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return _student.Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        
+    }
+}
